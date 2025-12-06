123a493beadadbe70543d4448569066becec693a
SoultaneRaqi <soultaneraqi1@gmail.com>
2025-12-06 11:14:36 +0100

    modifing in main.jsx and App.jsx and handling pages logic

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import ProductsList from "./pages/ProductsList";
+import AddProduct from "./pages/AddProduct";
+
+export default function App() {
+  const [page, setPage] = useState("dashboard");
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar setPage={setPage} />
+
+      <div className="p-6">
+        {page === "dashboard" && <Dashboard />}
+        {page === "list" && <ProductsList />}
+        {page === "add" && <AddProduct />}
+      </div>
+    </div>
+  );
+}
